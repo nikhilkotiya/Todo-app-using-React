01d2c253c611ddac1f2e23c669f586eb5118ef8b
nikhilkotiya <nikhilkotiya8@gmail.com>
2021-05-31 18:46:14 +0530

    first commit

---
+++ src/My_components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisici
+                ng elit. Nostrum excepturi libero accusantium, 
+                deserunt unde vero rem numquam, iure rerum inventore 
+                nihil ullam quibusdam a perferendis iste laborum? Magni, illum nemo?
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto eius 
+            alias dolorem laudantium saepe dolor fugiat quas hic vitae. Qui adipisc
+            i voluptatem quis incidunt exercitationem hic nemo quo provident unde?
+            </p>     
+            </div>
+    )
+}
