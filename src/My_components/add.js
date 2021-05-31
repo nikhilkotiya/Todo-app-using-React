d99ddff1c0d6aaeb1f7c9f95a7313170a5397a80
nikhilkotiya <nikhilkotiya8@gmail.com>
2021-05-31 14:23:14 +0530

    first commit

--- src/My_components/add.js
+++ src/My_components/add.js
@@ -6,19 +6,22 @@ export const AddTodo=(props)=> {
     const [desc, setDesc] = useState("")
     const submit=(e)=>{
         e.preventDefault();
-         
+        if(!title||!desc){
+            alert("title requred")
+        }
+        props.addtodo(title,desc);
     }
     return (
         <>
         <h1>Add Todo</h1>
         <form onSubmit={submit}>
             <div className="mb-3">
-                <label for="exampleInputEmail1" className="form-label">Title</label>
+                <label htmlFor="exampleInputEmail1" className="form-label">Title</label>
                 <input type="text"value={title} onChange={(e)=>{setTitle(e.target.value)}} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                 <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div className="mb-3">
-                <label for="exampleInputPassword1"  className="form-label">Discription</label>
+                <label htmlFor="exampleInputPassword1"  className="form-label">Discription</label>
                 <input type="text" value={desc} className="form-control" required onChange={(e)=>{setDesc(e.target.value)}} id="exampleInputPassword1" />
             </div>
             <button type="submit" className="btn btn-primary">Submit</button>
