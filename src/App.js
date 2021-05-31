8de9924db1e994efa90db92acaa54c67240055c6
nikhilkotiya <nikhilkotiya8@gmail.com>
2021-05-31 18:46:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,82 @@
 import './App.css';
+import About from './My_components/About'
 import Header from './My_components/Header'
 import Todo from './My_components/Todo'
-import React, { useState } from 'react';
-import {AddTodo} from './My_components/add'
+import React, { useState, useEffect } from 'react';
+import { AddTodo } from './My_components/add'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
-  const onDelete=(todo)=>{
-    console.log("I am on delete",todo)
-    setTodos(todos.filter((e)=>{
-      return e!==todo; 
-    }))
+  let initodo;
+  if (localStorage.getItem("todos") === null) {
+    initodo=[];
   }
-  const addtodo=(title,desc)=>{
-    console.log("i am on add",title,desc)
+  else {
+    initodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+
+  const onDelete = (todo) => {
+    console.log("I am on delete", todo)
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+   
+  const addtodo = (title, desc) => {
+    console.log("i am on add", title, desc)
     let count
-    if(todos.length == 0){
+    if (todos.length == 0) {
       console.log("i am here")
-      count =1;
+      count = 1;
     }
-    else{
+    else {
       console.log("i am here 2")
-      count = todos[todos.length -1].sno+1;
+      count = todos[todos.length - 1].sno + 1;
     }
     console.log(count)
-    const myTodo={
-      sno:count,
-      title:title,
-      discription:desc,
+    const myTodo = {
+      sno: count,
+      title: title,
+      discription: desc,
     }
-    console.log(myTodo)
-    setTodos([...todos,myTodo]);
-}
-  const [todos, setTodos] = useState([
-      {
-        sno:1,
-        title:"Go",
-        discription:"working",
-      }
-    ]);
-    return (
-    <>
-  <Header title="MyTodosList" />
-  <AddTodo addtodo={addtodo}/>
-  <Todo todos={todos}  onDelete={onDelete}/>
-  </>
+    setTodos([...todos, myTodo]);
+  }
+
+
+
+
+  const [todos, setTodos] = useState(initodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+
+
+  return (
+    <Router>
+      <Header title="MyTodosList" />
+      <Switch>
+          <Route exact path="/" render={()=>{
+            return(
+              <>
+              <AddTodo addtodo={addtodo} />
+              <Todo todos={todos} onDelete={onDelete} />
+            </>
+            )
+          }} > 
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 export default App;
